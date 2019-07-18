87c2b2ff855e9e2da6b6df93cf5a03bcdf7fe0b1
orngefrost <frostornge@gmail.com>
2019-07-19 00:38:37 +0900

    proto: Add `get deployments` methods

---
+++ proto/deployment.go
@@ -0,0 +1,62 @@
+package proto
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+)
+
+type Deployments map[string]abi.ABI
+type rawData map[string]map[string]interface{}
+
+func parseDeployments(rawData rawData) (Deployments, error) {
+	deployments := make(Deployments, len(rawData))
+
+	for contractName, contractInfo := range rawData {
+		rawAbi, err := json.Marshal(contractInfo["abi"])
+		if err != nil {
+			return nil, err
+		}
+
+		parsedAbi, err := abi.JSON(bytes.NewReader(rawAbi))
+		if err != nil {
+			return nil, err
+		}
+
+		deployments[contractName] = parsedAbi
+	}
+
+	return deployments, nil
+}
+
+func GetDeploymentsFromFile(path string) (Deployments, error) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	rawData := make(rawData)
+	if err := json.Unmarshal(data, &rawData); err != nil {
+		return nil, err
+	}
+
+	return parseDeployments(rawData)
+}
+
+func GetDeploymentsFromUrl(url string) (Deployments, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	rawData := make(rawData)
+	if err := json.NewDecoder(resp.Body).Decode(&rawData); err != nil {
+		return nil, err
+	}
+
+	return parseDeployments(rawData)
+}
