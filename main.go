4c39bc92b14da79850217ff53c9cbf4e0be0d5f2
hongbyeongsu <frostornge@gmail.com>
2019-07-19 16:10:59 +0900

    main: Make useless

--- main.go
+++ main.go
@@ -1,21 +1,3 @@
 package main
 
-import (
-	"log"
-
-	"github.com/frostornge/sol2proto/proto"
-)
-
-func main() {
-	deployments, err := proto.GetDeploymentsFromUrl("http://localhost:8500")
-	if err != nil {
-		panic(err)
-	}
-	contracts := proto.Parse(deployments)
-
-	for _, contract := range contracts {
-		if contract.PackageName == "accounts" {
-			log.Println(proto.Render(contract))
-		}
-	}
-}
+func main() {}
