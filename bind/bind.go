957ee6ffb5aca70af9f6afc028e677884b7281a1
orngefrost <frostornge@gmail.com>
2019-07-21 16:17:49 +0900

    bind: Remove java & split template source

--- bind/bind.go
+++ bind/bind.go
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-// Package bind generates Ethereum contract Go bindings.
+// Package bind generates Ethereum tmplContract Go bindings.
 //
 // Detailed usage document and tutorial available on the go-ethereum Wiki page:
 // https://github.com/ethereum/go-ethereum/wiki/Native-DApps:-Go-bindings-to-Ethereum-contracts
@@ -37,14 +37,6 @@ import (
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/frostornge/solgen/deployments"
-)
-
-// Lang is a target programming language selector to generate bindings for.
-type Lang int
-
-const (
-	LangGo Lang = iota
-	LangJava
 )
 
 func GenerateBind(path string, deployments deployments.Deployments) error {
@@ -74,15 +66,14 @@ func GenerateBind(path string, deployments deployments.Deployments) error {
 	return nil
 }
 
-// Bind generates a Go wrapper around a contract ABI. This wrapper isn't meant
+// Bind generates a Go wrapper around a tmplContract ABI. This wrapper isn't meant
 // to be used as is in client code, but rather as an intermediate struct which
 // enforces compile time type safety and naming convention opposed to having to
 // manually maintain hard coded strings that break on runtime.
 func Bind(name string, evmABI abi.ABI, pkg string) ([]byte, error) {
 	log.SetFlags(log.Llongfile)
-	lang := LangGo
 
-	// Process each individual contract requested binding
+	// Process each individual tmplContract requested binding
 	contracts := make(map[string]*tmplContract)
 
 	abiByte, err := json.Marshal(evmABI)
@@ -106,9 +97,9 @@ func Bind(name string, evmABI abi.ABI, pkg string) ([]byte, error) {
 		events    = make(map[string]*tmplEvent)
 	)
 	for _, original := range evmABI.Methods {
-		// Normalize the method for capital cases and non-anonymous inputs/outputs
+		// Normalize the tmplMethod for capital cases and non-anonymous inputs/outputs
 		normalized := original
-		normalized.Name = methodNormalizer[lang](original.Name)
+		normalized.Name = capitalise(original.Name)
 
 		normalized.Inputs = make([]abi.Argument, len(original.Inputs))
 		copy(normalized.Inputs, original.Inputs)
@@ -136,9 +127,9 @@ func Bind(name string, evmABI abi.ABI, pkg string) ([]byte, error) {
 		if original.Anonymous {
 			continue
 		}
-		// Normalize the event for capital cases and non-anonymous outputs
+		// Normalize the tmplEvent for capital cases and non-anonymous outputs
 		normalized := original
-		normalized.Name = methodNormalizer[lang](original.Name)
+		normalized.Name = capitalise(original.Name)
 
 		normalized.Inputs = make([]abi.Argument, len(original.Inputs))
 		copy(normalized.Inputs, original.Inputs)
@@ -150,7 +141,7 @@ func Bind(name string, evmABI abi.ABI, pkg string) ([]byte, error) {
 				}
 			}
 		}
-		// Append the event to the accumulator list
+		// Append the tmplEvent to the accumulator list
 		events[original.Name] = &tmplEvent{Original: original, Normalized: normalized}
 	}
 	contracts[name] = &tmplContract{
@@ -161,7 +152,7 @@ func Bind(name string, evmABI abi.ABI, pkg string) ([]byte, error) {
 		Transacts:   transacts,
 		Events:      events,
 	}
-	// Generate the contract template data content and render it
+	// Generate the tmplContract template data content and render it
 	data := &tmplData{
 		Package:   pkg,
 		Contracts: contracts,
@@ -169,13 +160,13 @@ func Bind(name string, evmABI abi.ABI, pkg string) ([]byte, error) {
 	buffer := new(bytes.Buffer)
 
 	funcs := map[string]interface{}{
-		"bindtype":      bindType[lang],
-		"bindtopictype": bindTopicType[lang],
-		"namedtype":     namedType[lang],
+		"bindtype":      bindType,
+		"bindtopictype": bindTopicType,
+		"namedtype":     func(string, abi.Type) string { panic("this shouldn't be needed") },
 		"capitalise":    capitalise,
 		"decapitalise":  decapitalise,
 	}
-	tmpl := template.Must(template.New("").Funcs(funcs).Parse(tmplSource[lang]))
+	tmpl := template.Must(template.New("").Funcs(funcs).Parse(tmplSource))
 	if err := tmpl.Execute(buffer, data); err != nil {
 		return []byte{}, err
 	}
@@ -186,13 +177,6 @@ func Bind(name string, evmABI abi.ABI, pkg string) ([]byte, error) {
 		return []byte{}, fmt.Errorf("%v\n%s", err, buffer)
 	}
 	return code, nil
-}
-
-// bindType is a set of type binders that convert Solidity types to some supported
-// programming language types.
-var bindType = map[Lang]func(kind abi.Type) string{
-	LangGo:   bindTypeGo,
-	LangJava: bindTypeJava,
 }
 
 // Helper function for the binding generators.
@@ -206,7 +190,7 @@ var bindType = map[Lang]func(kind abi.Type) string{
 func wrapArray(stringKind string, innerLen int, innerMapping string) (string, []string) {
 	remainder := stringKind[innerLen:]
 	//find all the sizes
-	matches := regexp.MustCompile(`\[(\d*)\]`).FindAllStringSubmatch(remainder, -1)
+	matches := regexp.MustCompile(`\[(\d*)]`).FindAllStringSubmatch(remainder, -1)
 	parts := make([]string, 0, len(matches))
 	for _, match := range matches {
 		//get group 1 from the regex match
@@ -217,7 +201,7 @@ func wrapArray(stringKind string, innerLen int, innerMapping string) (string, []
 
 // Translates the array sizes to a Go-lang declaration of a (nested) array of the inner type.
 // Simply returns the inner type if arraySizes is empty.
-func arrayBindingGo(inner string, arraySizes []string) string {
+func arrayBinding(inner string, arraySizes []string) string {
 	out := ""
 	//prepend all array sizes, from outer (end arraySizes) to inner (start arraySizes)
 	for i := len(arraySizes) - 1; i >= 0; i-- {
@@ -230,16 +214,16 @@ func arrayBindingGo(inner string, arraySizes []string) string {
 // bindTypeGo converts a Solidity type to a Go one. Since there is no clear mapping
 // from all Solidity types to Go ones (e.g. uint17), those that cannot be exactly
 // mapped will use an upscaled type (e.g. *big.Int).
-func bindTypeGo(kind abi.Type) string {
+func bindType(kind abi.Type) string {
 	stringKind := kind.String()
-	innerLen, innerMapping := bindUnnestedTypeGo(stringKind)
-	return arrayBindingGo(wrapArray(stringKind, innerLen, innerMapping))
+	innerLen, innerMapping := bindUnnestedType(stringKind)
+	return arrayBinding(wrapArray(stringKind, innerLen, innerMapping))
 }
 
 // The inner function of bindTypeGo, this finds the inner type of stringKind.
 // (Or just the type itself if it is not an array or slice)
 // The length of the matched part is returned, with the translated type.
-func bindUnnestedTypeGo(stringKind string) (int, string) {
+func bindUnnestedType(stringKind string) (int, string) {
 
 	switch {
 	case strings.HasPrefix(stringKind, "address"):
@@ -268,152 +252,14 @@ func bindUnnestedTypeGo(stringKind string) (int, string) {
 	}
 }
 
-// Translates the array sizes to a Java declaration of a (nested) array of the inner type.
-// Simply returns the inner type if arraySizes is empty.
-func arrayBindingJava(inner string, arraySizes []string) string {
-	// Java array type declarations do not include the length.
-	return inner + strings.Repeat("[]", len(arraySizes))
-}
-
-// bindTypeJava converts a Solidity type to a Java one. Since there is no clear mapping
-// from all Solidity types to Java ones (e.g. uint17), those that cannot be exactly
-// mapped will use an upscaled type (e.g. BigDecimal).
-func bindTypeJava(kind abi.Type) string {
-	stringKind := kind.String()
-	innerLen, innerMapping := bindUnnestedTypeJava(stringKind)
-	return arrayBindingJava(wrapArray(stringKind, innerLen, innerMapping))
-}
-
-// The inner function of bindTypeJava, this finds the inner type of stringKind.
-// (Or just the type itself if it is not an array or slice)
-// The length of the matched part is returned, with the translated type.
-func bindUnnestedTypeJava(stringKind string) (int, string) {
-
-	switch {
-	case strings.HasPrefix(stringKind, "address"):
-		parts := regexp.MustCompile(`address(\[[0-9]*\])?`).FindStringSubmatch(stringKind)
-		if len(parts) != 2 {
-			return len(stringKind), stringKind
-		}
-		if parts[1] == "" {
-			return len("address"), "Address"
-		}
-		return len(parts[0]), "Addresses"
-
-	case strings.HasPrefix(stringKind, "bytes"):
-		parts := regexp.MustCompile(`bytes([0-9]*)`).FindStringSubmatch(stringKind)
-		if len(parts) != 2 {
-			return len(stringKind), stringKind
-		}
-		return len(parts[0]), "byte[]"
-
-	case strings.HasPrefix(stringKind, "int") || strings.HasPrefix(stringKind, "uint"):
-		//Note that uint and int (without digits) are also matched,
-		// these are size 256, and will translate to BigInt (the default).
-		parts := regexp.MustCompile(`(u)?int([0-9]*)`).FindStringSubmatch(stringKind)
-		if len(parts) != 3 {
-			return len(stringKind), stringKind
-		}
-
-		namedSize := map[string]string{
-			"8":  "byte",
-			"16": "short",
-			"32": "int",
-			"64": "long",
-		}[parts[2]]
-
-		//default to BigInt
-		if namedSize == "" {
-			namedSize = "BigInt"
-		}
-		return len(parts[0]), namedSize
-
-	case strings.HasPrefix(stringKind, "bool"):
-		return len("bool"), "boolean"
-
-	case strings.HasPrefix(stringKind, "string"):
-		return len("string"), "String"
-
-	default:
-		return len(stringKind), stringKind
-	}
-}
-
-// bindTopicType is a set of type binders that convert Solidity types to some
-// supported programming language topic types.
-var bindTopicType = map[Lang]func(kind abi.Type) string{
-	LangGo:   bindTopicTypeGo,
-	LangJava: bindTopicTypeJava,
-}
-
-// bindTypeGo converts a Solidity topic type to a Go one. It is almost the same
+// bindType converts a Solidity topic type to a Go one. It is almost the same
 // funcionality as for simple types, but dynamic types get converted to hashes.
-func bindTopicTypeGo(kind abi.Type) string {
-	bound := bindTypeGo(kind)
+func bindTopicType(kind abi.Type) string {
+	bound := bindType(kind)
 	if bound == "string" || bound == "[]byte" {
 		bound = "common.Hash"
 	}
 	return bound
-}
-
-// bindTypeGo converts a Solidity topic type to a Java one. It is almost the same
-// funcionality as for simple types, but dynamic types get converted to hashes.
-func bindTopicTypeJava(kind abi.Type) string {
-	bound := bindTypeJava(kind)
-	if bound == "String" || bound == "Bytes" {
-		bound = "Hash"
-	}
-	return bound
-}
-
-// namedType is a set of functions that transform language specific types to
-// named versions that my be used inside method names.
-var namedType = map[Lang]func(string, abi.Type) string{
-	LangGo:   func(string, abi.Type) string { panic("this shouldn't be needed") },
-	LangJava: namedTypeJava,
-}
-
-// namedTypeJava converts some primitive data types to named variants that can
-// be used as parts of method names.
-func namedTypeJava(javaKind string, solKind abi.Type) string {
-	switch javaKind {
-	case "byte[]":
-		return "Binary"
-	case "byte[][]":
-		return "Binaries"
-	case "string":
-		return "String"
-	case "string[]":
-		return "Strings"
-	case "boolean":
-		return "Bool"
-	case "boolean[]":
-		return "Bools"
-	case "BigInt[]":
-		return "BigInts"
-	default:
-		parts := regexp.MustCompile(`(u)?int([0-9]*)(\[[0-9]*\])?`).FindStringSubmatch(solKind.String())
-		if len(parts) != 4 {
-			return javaKind
-		}
-		switch parts[2] {
-		case "8", "16", "32", "64":
-			if parts[3] == "" {
-				return capitalise(fmt.Sprintf("%sint%s", parts[1], parts[2]))
-			}
-			return capitalise(fmt.Sprintf("%sint%ss", parts[1], parts[2]))
-
-		default:
-			return javaKind
-		}
-	}
-}
-
-// methodNormalizer is a name transformer that modifies Solidity method names to
-// conform to target language naming concentions.
-var methodNormalizer = map[Lang]func(string) string{
-	LangGo:   capitalise,
-	LangJava: decapitalise,
 }
 
 // capitalise makes a camel-case string which starts with an upper case character.
